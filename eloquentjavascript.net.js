f7607013c86eb2df347117f819969b302021223b
Maximilian Schoening <mschoening@me.com>
2012-06-24 05:29:24 +0200

    First commit

---
+++ eloquentjavascript.net.js
@@ -0,0 +1,6 @@
+// A more readable Eloquent Javascript
+//
+// Preview:
+// http://cl.ly/2x123x2S1B1M2W373H3s
+
+$('.content').css('margin', '0 auto');
---
+++ news.ycombinator.com.js
@@ -0,0 +1,22 @@
+// A more readable Hacker News
+//
+// Inspired by:
+// https://github.com/zeke/dotjs_scripts/blob/master/news.ycombinator.com.js
+//
+// Preview:
+// http://cl.ly/HM0N
+
+$('table').css({
+  width: '700px',
+  background: 'none'
+});
+
+$('textarea').css({
+  width: '100%'
+});
+
+$('span.comment').css({
+  fontFamily: 'TisaCompPro',
+  fontSize: '1.2em',
+  lineHeight: '1.4em'
+});
