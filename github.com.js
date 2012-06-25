37a1f215957c2cd6b3bd572070c1fb5c4d6b79b3
Maximilian Schoening <mschoening@me.com>
2012-06-26 01:50:11 +0200

    Quiet Github the hell down

---
+++ github.com.js
@@ -0,0 +1,12 @@
+// Hide all the news, since you can't filter it
+//
+// Preview:
+// http://cl.ly/2I3R1P3N1W142f251r1p
+
+$('#dashboard .news').css('display', 'none');
+$('#dashboard .octofication').css('display', 'none');
+
+
+$('#dashboard .repos')
+  .css('box-sizing', 'border-box')
+  .css('width', '100%');
