0fbad0747c0c2b726545e3004ec88bb1767461a9
Maximilian Schoening <mschoening@me.com>
2012-06-26 02:10:51 +0200

    All the searchboxes for the Githubs

--- github.com.js
+++ github.com.js
@@ -9,3 +9,9 @@ $('#dashboard .octofication').css('display', 'none');
 $('#dashboard .repos')
   .css('box-sizing', 'border-box')
   .css('width', '100%');
+
+$('#dashboard .filter_input')
+  .css('border-radius', '4px')
+  .css('font-size', '24px')
+  .css('padding', '8px 8px 6px')
+  .css('width', '100%');
