1afbdaaac0972ff48bae03ad18bfdb6eaf74a6aa
Maximilian Schoening <mschoening@me.com>
2012-07-13 16:02:41 +0200

    Replace HN gray arrow GIF with unicode character :burger:

--- news.ycombinator.com.js
+++ news.ycombinator.com.js
@@ -4,7 +4,7 @@
 // https://github.com/zeke/dotjs_scripts/blob/master/news.ycombinator.com.js
 //
 // Preview:
-// http://cl.ly/HM0N
+// http://cl.ly/I3o0
 
 $('table').css({
   width: '700px',
@@ -19,4 +19,12 @@ $('span.comment').css({
   fontFamily: 'TisaCompPro, Georgia',
   fontSize: '1.2em',
   lineHeight: '1.4em'
+});
+
+$('img[src$="grayarrow.gif"]').replaceWith('<span class="grayarrow">▲</span>');
+
+$('span.grayarrow').css({
+  color: 'grey',
+  fontSize: '0.8em',
+  padding: '0 0.5em'
 });
